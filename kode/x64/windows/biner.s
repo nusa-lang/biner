cda6a4d87f366bd06faa8a7b761086b8e354afec
fernaerell <fernaerell2020@gmail.com>
2026-04-18 21:46:45 +0700

    Mencoba menggunakan fungsi jumlah karakter untuk menghitung karakter string di data konstant dan menjadikan nya kode exit

--- kode/x64/windows/biner.s
+++ kode/x64/windows/biner.s
@@ -1,9 +1,22 @@
+.section .rodata
+    nama: .ascii "Fern Aerell\0"
+
+.section .text
+
+# awal() pub
 .global awal
 
 # keluar(kode_keluar ecx)
 .extern keluar
+# jumlah_karakter(rcx *) rax
+.extern jumlah_karakter
 
+# awal()
 awal:
-    movl $0, %ecx
-    call keluar
-    ret
+    leaq nama(%rip), %rcx # rcx = &nama
+    call jumlah_karakter  # jumlah_karakter(rcx) rax
+
+    movl %eax, %ecx       # ecx = eax (eax itu 32bit nya rax)
+    call keluar           # keluar(ecx)
+
+    ret                   # tidak mengembalikan
